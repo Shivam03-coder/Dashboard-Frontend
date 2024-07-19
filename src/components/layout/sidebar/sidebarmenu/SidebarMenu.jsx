2bbf0454e17dd2364464ef45ad77c1c70fa29d8f
shivam anand <shivam850anand@gmail.com>
2024-07-20 00:15:15 +0530

    .

--- src/components/layout/sidebar/sidebarmenu/SidebarMenu.jsx
+++ src/components/layout/sidebar/sidebarmenu/SidebarMenu.jsx
@@ -1,17 +1,19 @@
 import { Typography } from "@material-tailwind/react";
 import SidebarLinks from "./SidebarLinks";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const SidebarMenu = () => {
   return (
     <>
       <div className="mb-5 p-3 sticky top-0 z-20  bg-customeYellow text-black">
-        <NavLink to={"/dashboard"} className="opacity-90 my-auto text-center font-Varela text-3xl text-black font-semibold">
+        <Typography className="opacity-90 my-auto text-center font-Varela text-3xl text-black font-semibold">
           DASHBOARD
-        </NavLink>
+        </Typography>
       </div>
       <div className="md:hidden block">
-      <Typography variant="h3" className="text-center" >FINNANCE <br /> TRACKER</Typography>
+        <Typography variant="h3" className="text-center">
+          FINNANCE <br /> TRACKER
+        </Typography>
       </div>
       <div className="navigations px-5 space-y-7 mt-6 text-customeYellow">
         <SidebarLinks />
