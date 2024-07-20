59cad57e45e46bb5a2ea3e0d093d9a4579fe7049
Shivam Anand <shivam850anand@gmail.com>
2024-07-20 11:34:31 +0530

    Update Approutes.jsx

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -3,13 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import Root from "../components/Root";
 import Balltriangle from "../components/shared/loaders/Balltriangle";
 const Productpage = lazy(() => import("../components/pages/Productpage"));
-const Customerpage = lazy(() => import("../components/pages/Customerpage"));
-const Transcationspage = lazy(() =>
-  import("../components/pages/Transcationspage")
-);
-const OverviewPage = lazy(() => import("../components/pages/OverviewPage"));
-const Breakdownpage = lazy(() => import("../components/pages/Breakdownpage"));
-const Adminpage = lazy(() => import("../components/pages/Adminpage"));
+import Customerpage from "../components/pages/Customerpage";
+import Transcationspage from "../components/pages/Transcationspage";
+import OverviewPage from "../components/pages/OverviewPage";
+import Breakdownpage from "../components/pages/Breakdownpage";
+import Adminpage from "../components/pages/Adminpage";
 
 const Approutes = () => {
   return (
